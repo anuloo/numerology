7fa49e7e4c8d637eada9dac8c266e099d84d09ae
Max Walker <mxwalker770@gmail.com>
2026-03-13 04:28:32 +0000

    updated the vite

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     port: 5173,
 
     allowedHosts: [
-      'hall-sorry-along-highlights.trycloudflare.com'
+      'reproduced-amended-dealing-sheer.trycloudflare.com'
     ],
 
     hmr: {
